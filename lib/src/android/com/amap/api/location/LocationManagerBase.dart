22ab8f075649e2268a2171511e8bac12e7b65bce
yohom <382146139@qq.com>
2019-09-29 16:00:46 +0800

    enhancement: export文件android和ios单独处理, library级别的export需要手动export, 这样才合理

--- lib/src/android/com/amap/api/location/LocationManagerBase.dart
+++ lib/src/android/com/amap/api/location/LocationManagerBase.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
-import 'package:amap_search_fluttify/amap_search_fluttify.dart';
+import 'package:amap_search_fluttify/src/ios/ios.export.dart';
+import 'package:amap_search_fluttify/src/android/android.export.dart';
 import 'package:flutter/services.dart';
 
 // ignore_for_file: non_constant_identifier_names, camel_case_types
