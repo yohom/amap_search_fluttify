162521e5d3527981a25577cf0efcae2e7a771432
yohom <382146139@qq.com>
2019-09-29 16:48:12 +0800

    enhancement: 更新搜索demo

--- example/lib/get_map_data/get_poi.screen.dart
+++ example/lib/get_map_data/get_poi.screen.dart
@@ -1,4 +1,5 @@
 import 'package:amap_search_fluttify/amap_search_fluttify.dart';
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 
 class GetPoiScreen extends StatefulWidget {
@@ -7,25 +8,42 @@ class GetPoiScreen extends StatefulWidget {
 }
 
 class _GetPoiScreenState extends State<GetPoiScreen> {
-  @override
-  void initState() {
-    super.initState();
-    AmapSearch.search(
-      keyword: '金职院',
-      city: '金华',
-      onPoiSearched: (pois) async {
-        for (final poi in pois) {
-          print(poi.title);
-        }
-      },
-    );
-  }
+  final _keywordController = TextEditingController();
+  final _cityController = TextEditingController();
+
+  List<Poi> _poiList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(title: Text('GetPoi')),
-      body: Center(child: Text('GetPoi')),
+      body: DecoratedColumn(
+        padding: EdgeInsets.all(kSpaceBig),
+        children: <Widget>[
+          TextFormField(
+            controller: _keywordController,
+            decoration: InputDecoration(hintText: '输入关键字'),
+          ),
+          TextFormField(
+            controller: _cityController,
+            decoration: InputDecoration(hintText: '输入城市'),
+          ),
+          RaisedButton(
+            onPressed: () async {
+              final poiList = await AmapSearch.search(
+                keyword: _keywordController.text,
+                city: _cityController.text,
+              );
+              setState(() {
+                _poiList = poiList;
+              });
+            },
+            child: Text('搜索'),
+          ),
+          Text(_poiList.map((it) => it.title).join("\n")),
+        ],
+      ),
     );
   }
 }
