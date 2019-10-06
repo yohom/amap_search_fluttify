237ea6138566f0231bf7c4acbb819a1d9775ef36
yohom <382146139@qq.com>
2019-10-06 10:57:14 +0800

    enhancement: 增加忽略警告列表 +1

--- lib/src/ios/AMapRidingRouteSearchResponse.dart
+++ lib/src/ios/AMapRidingRouteSearchResponse.dart
@@ -4,7 +4,7 @@ import 'package:amap_search_fluttify/src/ios/ios.export.dart';
 import 'package:amap_search_fluttify/src/android/android.export.dart';
 import 'package:flutter/services.dart';
 
-// ignore_for_file: non_constant_identifier_names, camel_case_types
+// ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
 class AMapRidingRouteSearchResponse extends AMapRouteSearchResponse  {
   static final _channel = MethodChannel('me.yohom/amap_search_fluttify');
 
