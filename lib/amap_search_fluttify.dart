08bc0b4d7337e08828cac7cbc2e1f7235412b57e
yohom <382146139@qq.com>
2020-10-16 20:06:48 +0800

    enhance: export

--- lib/amap_search_fluttify.dart
+++ lib/amap_search_fluttify.dart
@@ -3,3 +3,5 @@ library amap_search_fluttify;
 export 'package:amap_core_fluttify/amap_core_fluttify.dart';
 
 export 'src/facade/amap_search.dart';
+export 'src/android/android.export.g.dart';
+export 'src/ios/ios.export.g.dart';
